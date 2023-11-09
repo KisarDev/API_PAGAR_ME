2b4fdcc479e48908e4f774acc90172b2fd6d10e4
unity-team-developers <cesarmartins.pro@gmail.com>
2023-11-09 11:47:03 +0000

    removing data_format on settings.py

--- core/settings.py
+++ core/settings.py
@@ -125,7 +125,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-REST_FRAMEWORK = {
-    "DATE_INPUT_FORMATS": ["%d-%m-%Y"],
-}

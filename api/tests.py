efba7b1bfc28a11b41f4d2be0e23706aedd83d71
unity-team-developers <cesarmartins.pro@gmail.com>
2023-11-26 17:37:24 +0000

    Teste de Registrar com sucesso e de falhar com campos faltando

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
-
-# Create your tests here.
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+class AccountTests(APITestCase):
+    def test_register_transaction(self):
+        """
+        Esse teste tenta registrar uma transação
+        """
+        url = reverse('register_transaction')
+        data = {
+                    "card_number": "9122-2222-2222-2222",
+                    "payment_value": 1200,
+                    "description_transaction": "academy",
+                    "payment_method": "D",
+                    "customer_name": "Alcione",
+                    "expiration_date": "2023-11-26",
+                    "cvv": 123
+                }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    def test_register_transaction_payment_value_null(self):
+        """
+        Esse teste tenta registrar uma transação
+        """
+        url = reverse('register_transaction')
+        data = {
+                    "card_number": "9122-2222-2222-2222",
+                    "description_transaction": "academy",
+                    "payment_method": "D",
+                    "customer_name": "Alcione",
+                    "expiration_date": "2023-11-26",
+                    "cvv": 123
+                }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    
--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from api.models import Payables, Transaction
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
 
 
 '''
@@ -25,20 +26,19 @@ def register_transaction(request):
        - Realiza operações para diferentes métodos de pagamento.
     """
     serializer = TransactionSerializer(data=request.data)
-    value = request.data['payment_value']
-    method = request.data['payment_method'] 
-    last_four_digits = request.data['card_number'][-4:]
-    masked_card_number = '*' * 15 + last_four_digits
-
-    request.data['card_number'] = masked_card_number
-
-
+    
     if serializer.is_valid():
+        value = request.data['payment_value']
+        method = request.data['payment_method'] 
+        last_four_digits = request.data['card_number'][-4:]
+        masked_card_number = '*' * 15 + last_four_digits
+        request.data['card_number'] = masked_card_number
+
         Transaction.value_return(value=value, method=method)
         serializer.save()
-        return Response('Transacao realizada com sucesso!!!')
+        return Response('Transacao realizada com sucesso!!!', status=status.HTTP_201_CREATED)
     else:
-        return Response(serializer.errors)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
 
 @swagger_auto_schema(method='get')
 @api_view(['GET'])
